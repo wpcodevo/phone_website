010037fd11aeea3f2d5f50423cf8fcc5fdc6ce0c
Ziddah Edem <ziddah.edem@gmail.com>
2020-08-27 09:36:22 +0000

    Initial Commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+/* 
+Navigation
+*/
 const navOpen = document.querySelector(".nav__hamburger");
 const navClose = document.querySelector(".close__toggle");
 const menu = document.querySelector(".nav__menu");
@@ -9,3 +12,16 @@ navOpen.addEventListener("click", () => {
 navClose.addEventListener("click", () => {
   menu.classList.remove("open");
 });
+
+/* 
+Glidejs Carousel
+*/
+new Glide("#glide_1", {
+  type: "carousel",
+  startAt: 0,
+  autoplay: 3000,
+  hoverpause: true,
+  perView: 1,
+  animationDuration: 800,
+  animationTimingFunc: "ease-in-out",
+}).mount();

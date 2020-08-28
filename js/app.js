5cf749ec47a568e1f8647612b1eac378c9079a28
Ziddah Edem <ziddah.edem@gmail.com>
2020-08-28 19:46:55 +0000

    initial commit

--- js/app.js
+++ js/app.js
@@ -61,3 +61,25 @@ if (slider2) {
     },
   }).mount();
 }
+// Latest Products
+if (slider3) {
+  new Glide("#glide_3", {
+    type: "carousel",
+    startAt: 0,
+    perView: 4,
+    rewin: false,
+    animationDuration: 800,
+    animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
+    breakpoints: {
+      1200: {
+        perView: 3,
+      },
+      768: {
+        perView: 2,
+      },
+      568: {
+        perView: 1,
+      },
+    },
+  }).mount();
+}

f622d5594b4528991affa67036efa43ec830619c
Ziddah Edem <ziddah.edem@gmail.com>
2020-08-30 18:35:12 +0000

    initial commit

--- js/products.js
+++ js/products.js
@@ -4,9 +4,9 @@ Filter
 =============
  */
 const filterBtn = document.querySelectorAll(".filter-btn");
-const titleSection = document.querySelectorAll(".section__title");
-const latestCenter = document.querySelector(".latest-center");
-const relatedCenter = document.querySelector(".related-center");
+// const titleSection = document.querySelectorAll(".section__title");
+// const latestCenter = document.querySelector(".latest-center");
+// const relatedCenter = document.querySelector(".related-center");
 const categoryCenter = document.querySelector(".category__center");
 
 /*
@@ -32,29 +32,26 @@ Latest Products Products
 =============
  */
 
-const latestProduct = async () => {
-  const products = await getProducts();
-  const latestProducts = products.filter(
-    product => product.category === "Latest Products"
-  );
-
-  displayProducts(latestProducts, latestCenter);
-  console.log(latestProducts);
-};
+// const latestProduct = async () => {
+//   const products = await getProducts();
+//   const latestProducts = products.filter(
+//     product => product.category === "Latest Products"
+//   );
+//   return latestProducts;
+// };
 
 /*
 =============
 Related Products Products
 =============
  */
-const relatedProduct = async () => {
-  const products = await getProducts();
-  const relatedProducts = products.filter(
-    product => product.category === "Related Products"
-  );
-
-  displayProducts(relatedProducts, relatedCenter);
-};
+// const relatedProduct = async () => {
+//   const products = await getProducts();
+//   const relatedProducts = products.filter(
+//     product => product.category === "Related Products"
+//   );
+//   return relatedProducts;
+// };
 
 /*
 =============
@@ -64,50 +61,52 @@ Load Category Products
 window.addEventListener("DOMContentLoaded", async function () {
   const products = await getProducts();
   displayProductItems(products);
-  latestProduct();
-  relatedProduct();
+  // const relatedProducts = await relatedProduct();
+  // const latestProducts = await latestProduct();
+  // displayProducts(latestProducts, latestCenter);
+  // displayProducts(relatedProducts, relatedCenter);
 });
 
-const displayProducts = (items, category) => {
-  let displayProduct = items.map(
-    product => ` 
-                  <li class="glide__slide">
-                  <div class="product">
-                    <div class="product__header">
-                      <a href="#"><img src=${product.image} alt="product"></a>
-                    </div>
-                    <div class="product__footer">
-                      <h3>${product.name}</h3>
-                      <div class="rating">
-                        <svg>
-                          <use xlink:href="./images/sprite.svg#icon-star-full"></use>
-                        </svg>
-                        <svg>
-                          <use xlink:href="./images/sprite.svg#icon-star-full"></use>
-                        </svg>
-                        <svg>
-                          <use xlink:href="./images/sprite.svg#icon-star-full"></use>
-                        </svg>
-                        <svg>
-                          <use xlink:href="./images/sprite.svg#icon-star-full"></use>
-                        </svg>
-                        <svg>
-                          <use xlink:href="./images/sprite.svg#icon-star-empty"></use>
-                        </svg>
-                      </div>
-                      <div class="product__price">
-                        <span>$${product.price}</span>
-                      </div>
-                      <a href="#"><button type="submit" class="product__btn">Add To Cart</button></a>
-                    </div>
-                  </div>
-                </li>
-                  `
-  );
-
-  displayProduct = displayProduct.join("");
-  category.innerHTML = displayProduct;
-};
+// const displayProducts = (items, productCenter) => {
+//   let displayProduct = items.map(
+//     product => `
+//                   <li class="glide__slide">
+//                   <div class="product">
+//                     <div class="product__header">
+//                       <a href="#"><img src=${product.image} alt="product"></a>
+//                     </div>
+//                     <div class="product__footer">
+//                       <h3>${product.name}</h3>
+//                       <div class="rating">
+//                         <svg>
+//                           <use xlink:href="./images/sprite.svg#icon-star-full"></use>
+//                         </svg>
+//                         <svg>
+//                           <use xlink:href="./images/sprite.svg#icon-star-full"></use>
+//                         </svg>
+//                         <svg>
+//                           <use xlink:href="./images/sprite.svg#icon-star-full"></use>
+//                         </svg>
+//                         <svg>
+//                           <use xlink:href="./images/sprite.svg#icon-star-full"></use>
+//                         </svg>
+//                         <svg>
+//                           <use xlink:href="./images/sprite.svg#icon-star-empty"></use>
+//                         </svg>
+//                       </div>
+//                       <div class="product__price">
+//                         <span>$${product.price}</span>
+//                       </div>
+//                       <a href="#"><button type="submit" class="product__btn">Add To Cart</button></a>
+//                     </div>
+//                   </div>
+//                 </li>
+//                   `
+//   );
+//   displayProduct = displayProduct.join("");
+//   console.log(displayProduct);
+//   productCenter.innerHTML = displayProduct;
+// };
 
 const displayProductItems = items => {
   let displayProduct = items.map(
